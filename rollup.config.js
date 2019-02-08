64029f0755f095ac21a909085ae5cbc28d1cc3d8
Marcelo Jr <marceloadsj1@gmail.com>
2019-02-08 13:50:52 +0100

    adding create-react-hook project with some starting hooks

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,36 @@
-import babel from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-commonjs'
-import external from 'rollup-plugin-peer-deps-external'
-import resolve from 'rollup-plugin-node-resolve'
-import url from 'rollup-plugin-url'
+import babel from "rollup-plugin-babel";
+import commonjs from "rollup-plugin-commonjs";
+import external from "rollup-plugin-peer-deps-external";
+import resolve from "rollup-plugin-node-resolve";
+import url from "rollup-plugin-url";
 
-import pkg from './package.json'
+import pkg from "./package.json";
 
 export default {
-  input: 'src/index.js',
+  input: "src/index.js",
   output: [
     {
       file: pkg.main,
-      format: 'cjs',
+      format: "cjs",
       sourcemap: true
     },
     {
       file: pkg.module,
-      format: 'es',
+      format: "es",
       sourcemap: true
     }
   ],
   plugins: [
     external(),
-    url({ exclude: ['**/*.svg'] }),
+    url({ exclude: ["**/*.svg"] }),
     babel({
-      exclude: 'node_modules/**'
+      exclude: "node_modules/**"
     }),
     resolve(),
-    commonjs()
+    commonjs({
+      namedExports: {
+        "node_modules/indicative/builds/main.js": ["validate"]
+      }
+    })
   ]
-}
+};
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import { validate } from "indicative";
 
 function useValidate(data, rules, messages) {
-  const [error, setError] = useState();
+  const [errors, setErrors] = useState();
 
   useEffect(() => {
     validate(data, rules, messages)
-      .then(() => serError(undefined))
-      .catch(errors => setError(errors));
-  }, [value]);
+      .then(() => setErrors(undefined))
+      .catch(errors => setErrors(errors));
+  }, Object.values(data));
 
   return errors;
 }
@@ -16,6 +16,12 @@ function useValidate(data, rules, messages) {
 function useStateValidator(initialState, rules, messages) {
   const [value, setValue] = useState(initialState);
   const [error, setError] = useState();
+  const [touched, setTouched] = useState(false);
+
+  function setValueAndTouched(value) {
+    if (!touched) setTouched(true);
+    setValue(value);
+  }
 
   messages = useMemo(() => {
     if (messages) {
@@ -27,12 +33,14 @@ function useStateValidator(initialState, rules, messages) {
   }, [messages]);
 
   useEffect(() => {
-    validate({ value }, { value: rules }, messages)
-      .then(() => serError(undefined))
-      .catch(error => setError(error[0]));
+    if (touched) {
+      validate({ value }, { value: rules }, messages)
+        .then(() => serError(undefined))
+        .catch(errors => setError(errors[0]));
+    }
   }, [value]);
 
-  return [value, setValue, error];
+  return [value, setValueAndTouched, error];
 }
 
 export { useValidate, useStateValidator };

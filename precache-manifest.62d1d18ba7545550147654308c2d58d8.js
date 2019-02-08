561f7fd33b3e96b6204c7d18fd4ad1a4acef3e03
Marcelo Jr <marceloadsj1@gmail.com>
2019-02-08 23:54:57 +0100

    Updates

---
+++ precache-manifest.62d1d18ba7545550147654308c2d58d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "04c16e2d7fb151f2ea13",
+    "url": "/react-indicative-hooks/static/css/main.461b83d9.chunk.css"
+  },
+  {
+    "revision": "04c16e2d7fb151f2ea13",
+    "url": "/react-indicative-hooks/static/js/main.04c16e2d.chunk.js"
+  },
+  {
+    "revision": "c780f3e92a9b296ebda9",
+    "url": "/react-indicative-hooks/static/js/1.c780f3e9.chunk.js"
+  },
+  {
+    "revision": "198130e926f09143c034",
+    "url": "/react-indicative-hooks/static/js/runtime~main.198130e9.js"
+  },
+  {
+    "revision": "d6e00fe0d4ee8abe06cd621483853e26",
+    "url": "/react-indicative-hooks/index.html"
+  }
+];

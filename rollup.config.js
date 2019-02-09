37b971b4864a44ff405345815793a0aef996916a
Marcelo Jr <marceloadsj1@gmail.com>
2019-02-09 19:36:10 +0100

    Adding umd build to the pkg

--- rollup.config.js
+++ rollup.config.js
@@ -3,34 +3,56 @@ import commonjs from "rollup-plugin-commonjs";
 import external from "rollup-plugin-peer-deps-external";
 import resolve from "rollup-plugin-node-resolve";
 import url from "rollup-plugin-url";
+import { terser } from "rollup-plugin-terser";
 
 import pkg from "./package.json";
 
-export default {
-  input: "src/index.js",
-  output: [
-    {
+const input = "src/index.js";
+
+const plugins = [
+  external(),
+  url({ exclude: ["**/*.svg"] }),
+  babel({
+    exclude: "node_modules/**"
+  }),
+  resolve(),
+  commonjs({
+    namedExports: {
+      "node_modules/indicative/builds/main.js": ["validate", "validateAll"]
+    }
+  })
+];
+
+export default [
+  {
+    input,
+    output: {
       file: pkg.main,
       format: "cjs",
       sourcemap: true
     },
-    {
+    plugins
+  },
+  {
+    input,
+    output: {
       file: pkg.module,
       format: "es",
       sourcemap: true
-    }
-  ],
-  plugins: [
-    external(),
-    url({ exclude: ["**/*.svg"] }),
-    babel({
-      exclude: "node_modules/**"
-    }),
-    resolve(),
-    commonjs({
-      namedExports: {
-        "node_modules/indicative/builds/main.js": ["validate", "validateAll"]
+    },
+    plugins
+  },
+  {
+    input,
+    output: {
+      file: pkg.browser,
+      format: "umd",
+      sourcemap: true,
+      name: "reactIndicativeHooks",
+      globals: {
+        react: "React"
       }
-    })
-  ]
-};
+    },
+    plugins: [...plugins, terser()]
+  }
+];

d1a60af3f0c3fab1c253e1051f5abb5eaf642563
Marcelo Jr <marceloadsj1@gmail.com>
2019-02-09 18:52:01 +0100

    Adding code for the last missing component

--- example/src/molecules/ApiUseStateValidator.js
+++ example/src/molecules/ApiUseStateValidator.js
@@ -64,7 +64,47 @@ export default function SimpleInput() {
         <InputWithOptions />
       </div>
 
-      <CodeBlock comp>{``}</CodeBlock>
+      <CodeBlock comp>
+        {`import React, { useState } from "react";
+import { useStateValidator } from "react-indicative-hooks";
+
+const rules = "required";
+
+const messages = {
+  required: "Please, fill the input with some data"
+};
+
+export default function InputWithOptions() {
+  const [show, setShow] = useState(false);
+
+  const options = {
+    messages,
+    runOnMount: true,
+    onSuccess: () => setShow(false),
+    onError: () => setShow(true)
+  };
+
+  const [value, setValue, error] = useStateValidator("", rules, options);
+
+  return (
+    <>
+      <p className="text-red mb-5" data-testid="alert">
+        {show && "Sorry, you have an error"}
+      </p>
+      Value
+      <input
+        value={value}
+        onChange={e => setValue(e.target.value)}
+        data-testid="input"
+        className="ml-2 mr-5 mb-5"
+      />
+      <p className="text-red" data-testid="error">
+        {error && error.message}
+      </p>
+    </>
+  );
+}`}
+      </CodeBlock>
     </>
   );
 }
